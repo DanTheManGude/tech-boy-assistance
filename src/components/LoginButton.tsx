77b0b29d5aaf68f96a469ca218572489dd9aa473
DanTheManGude <dgude31@gmail.com>
2023-11-17 22:02:39 -0500

    kinda firebase

---
+++ src/components/LoginButton.tsx
@@ -0,0 +1,34 @@
+import { useAuth } from "@/context/AuthContext";
+
+const LoginButton = () => {
+  // Use the signIn method from the AuthContext
+  const { logIn } = useAuth();
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+    try {
+      await logIn();
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white p-4 py-8 shadow-md dark:border-gray-700 dark:bg-gray-800 sm:p-6 sm:py-10 md:p-8 md:py-14">
+        <h5 className="mb-2 text-center text-2xl font-medium text-gray-900 dark:text-white sm:text-3xl sm:font-semibold">
+          Login
+        </h5>
+
+        <button
+          onClick={handleLogin}
+          className="mb-8 mt-2 w-full rounded-lg bg-green-600 px-5 py-3 text-center text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:cursor-not-allowed disabled:bg-gradient-to-br disabled:from-gray-100 disabled:to-gray-300 disabled:text-gray-400 group-invalid:pointer-events-none group-invalid:bg-gradient-to-br group-invalid:from-gray-100 group-invalid:to-gray-300 group-invalid:text-gray-400 group-invalid:opacity-70"
+        >
+          Login to your account
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginButton;
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,82 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useRouter } from "next/navigation";
+
+import { auth } from "../firebase.config";
+
+// User data type interface
+interface UserType {
+  email: string | null;
+  uid: string | null;
+}
+
+// Create auth context
+const AuthContext = createContext({});
+
+// Make auth context available across the app by exporting it
+export const useAuth = () => useContext<any>(AuthContext);
+
+// Create the auth context provider
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  // Define the constants for the user and loading state
+  const [user, setUser] = useState<UserType>({ email: null, uid: null });
+  const [loading, setLoading] = useState<Boolean>(true);
+  const router = useRouter();
+
+  // Update the state depending on auth
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser({
+          email: user.email,
+          uid: user.uid,
+        });
+      } else {
+        setUser({ email: null, uid: null });
+      }
+    });
+
+    setLoading(false);
+
+    return () => unsubscribe();
+  }, []);
+
+  // Redirect to Dashboard if user exists
+  useEffect(() => {
+    if (user) {
+      router.push("/dashboard");
+    }
+  }, [user, router]);
+
+  // Login the user
+  const logIn = () => {
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({
+      prompt: "select_account",
+    });
+
+    return signInWithPopup(auth, provider);
+  };
+
+  // Logout the user
+  const logOut = async () => {
+    setUser({ email: null, uid: null });
+    return await signOut(auth);
+  };
+
+  // Wrap the children with the context provider
+  return (
+    <AuthContext.Provider value={{ user, logIn, logOut }}>
+      {loading ? null : children}
+    </AuthContext.Provider>
+  );
+};
