cafcbd94c048f9805f43e78ab2d6482694802a22
DanTheManGude <dgude31@gmail.com>
2023-12-03 15:52:30 -0500

    and ui of button

---
+++ src/components/PermissionButton.tsx
@@ -0,0 +1,24 @@
+import { useData, Message } from "@/context/DataContext";
+
+export default function PermissionButton() {
+  const { isAdmin } = useData();
+
+  if (!isAdmin) {
+    return null;
+  }
+
+  const handleClick = () => {};
+
+  return (
+    <div className="flex items-center justify-center mx-3 pt-3">
+      <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800">
+        <button
+          onClick={handleClick}
+          className="w-full rounded-lg bg-orange-600 px-5 py-3 text-center text-sm font-medium text-white hover:bg-orange-700 focus:outline-none focus:ring-4 focus:ring-orange-300"
+        >
+          Request Notification Permission
+        </button>
+      </div>
+    </div>
+  );
+}
