e2994e9ce5d5abd2b4cbff0f20e4d1af6f726daa
DanTheManGude <dgude31@gmail.com>
2023-12-03 16:53:36 -0500

    service worker and requesting token

--- src/components/PermissionButton.tsx
+++ src/components/PermissionButton.tsx
@@ -1,4 +1,12 @@
-import { useData, Message } from "@/context/DataContext";
+import { useData } from "@/context/DataContext";
+
+const requestPermission = () => {
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      console.log("Notification permission granted.");
+    }
+  });
+};
 
 export default function PermissionButton() {
   const { isAdmin } = useData();
@@ -7,13 +15,11 @@ export default function PermissionButton() {
     return null;
   }
 
-  const handleClick = () => {};
-
   return (
     <div className="flex items-center justify-center mx-3 pt-3">
       <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800">
         <button
-          onClick={handleClick}
+          onClick={requestPermission}
           className="w-full rounded-lg bg-orange-600 px-5 py-3 text-center text-sm font-medium text-white hover:bg-orange-700 focus:outline-none focus:ring-4 focus:ring-orange-300"
         >
           Request Notification Permission
--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,5 +1,6 @@
-import { getApps, initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -10,8 +11,24 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-if (!getApps().length) {
-  initializeApp(firebaseConfig);
-}
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
+export const messaging = getMessaging(app);
+
+getToken(messaging, {
+  vapidKey: process.env.NEXT_PUBLIC_VAPID_KEY,
+})
+  .then((currentToken) => {
+    if (currentToken) {
+      console.log(currentToken);
+    } else {
+      console.log(
+        "No registration token available. Request permission to generate one."
+      );
+    }
+  })
+  .catch((err) => {
+    console.log("An error occurred while retrieving token. ", err);
+    // ...
+  });
