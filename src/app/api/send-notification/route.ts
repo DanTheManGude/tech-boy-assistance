1e65056161b04b56ed093c761b5a81780aa46083
DanTheManGude <dgude31@gmail.com>
2023-12-03 21:03:22 -0500

    Send message with Admin sdk

--- src/app/api/send-notification/route.ts
+++ src/app/api/send-notification/route.ts
@@ -1,12 +1,39 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import Payload from "./payloadType";
+import Data from "./payloadType";
+import admin from "firebase-admin";
+
+var serviceAccount = require("./service-account-file.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tech-boy-assistance-default-rtdb.firebaseio.com",
+});
+
+const messaging = admin.messaging();
 
 export async function POST(request: NextRequest) {
-  const data: Payload = await request.json();
+  const data: Data = await request.json();
   const { fcmToken, fromName, reason } = data;
 
   console.log(fcmToken, fromName, reason);
+
+  var payload = {
+    notification: {
+      title: `New request from ${fromName}`,
+      body: `Reason: ${reason}`,
+    },
+    token: fcmToken,
+  };
+
+  messaging
+    .send(payload)
+    .then((result) => {
+      console.log("messaging send result", result);
+    })
+    .catch((error) => {
+      console.error("messaging send error", error);
+    });
 
   return new NextResponse(null, { status: 204 });
 }
