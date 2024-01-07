ac6e7f38821d2d179a53c09f24a6e9ca4dd1c090
DanTheManGude <dgude31@gmail.com>
2024-01-07 16:52:58 -0500

    nit warning on props of select

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -63,13 +63,13 @@ export default function MessageList() {
         <select
           id="status"
           className="my-2 w-5/6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+          onChange={(event) => {
             handleChangeStatus(event.target.value, key);
           }}
           value={status}
         >
           {Object.values(messageStatusKeys).map((statusValue) => (
-            <option value={statusValue} selected={statusValue === status}>
+            <option value={statusValue} key={statusValue}>
               {messageStatusMap[statusValue]}
             </option>
           ))}
